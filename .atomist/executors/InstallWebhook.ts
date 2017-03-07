7fca758d597329048b777accee9939f396a57be6
Christian Dupuis <cd@atomist.com>
2017-03-07 22:11:20 +0100

    Add url parameter for installwebhook

--- .atomist/executors/InstallWebhook.ts
+++ .atomist/executors/InstallWebhook.ts
@@ -7,25 +7,25 @@ import { GitHubService } from "@atomist/github/core/Core"
 import { Owner, Repository, RepoHookUserToken, OrgHookUserToken } from './Parameters'
 
 interface Parameters {
+  url: string
   owner: string
   repo: string
   token: string
 }
-
-let url = "https://webhook.atomist.com/github"
 
 export let installOrgWebhook: Executor = {
     description: "Install organization level webhook",
     name: "InstallOrgWebhook",
     tags: ["atomist/intent=install org-webhook", "atomist/private=false"],
     parameters: [
+        { name: "url", description: "URL of the Webhook to be installed", pattern: "@url", required: true, tags: ["atomist/webhook_url"]},
         Owner, OrgHookUserToken
     ],
     execute(services: Services, p: Parameters): Result {
 
         let _services: any = services
         let githubService = _services.github() as GitHubService
-        let status = githubService.installWebhook(url, p.owner, null, p.token)
+        let status = githubService.installWebhook(p.url, p.owner, null, p.token)
         _services.messageBuilder().say(status.message()).send()
         if (status.success()) {
             return new Result(Status.Success, "OK")
@@ -41,13 +41,14 @@ export let installRepoWebhook: Executor = {
     name: "InstallRepoWebhook",
     tags: ["atomist/intent=install webhook", "atomist/private=false"],
     parameters: [
+        { name: "url", description: "URL of the Webhook to be installed", pattern: "@url", required: true, tags: ["atomist/webhook_url"]},
         Owner, Repository, RepoHookUserToken
     ],
     execute(services: Services, p: Parameters): Result {
 
         let _services: any = services
         let githubService = _services.github() as GitHubService
-        let status = githubService.installWebhook(url, p.owner, p.repo, p.token)
+        let status = githubService.installWebhook(p.url, p.owner, p.repo, p.token)
         _services.messageBuilder().say(status.message()).send()
         if (status.success()) {
             return new Result(Status.Success, "OK")
