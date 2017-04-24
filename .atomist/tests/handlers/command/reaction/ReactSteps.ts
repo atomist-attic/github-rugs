6250b8f543c981e55db7c8152c3a9ae8cccc31d5
David Dooling <david@atomist.com>
2017-04-24 14:03:52 -0500

    Updates for rug 0.26.1 and @atomist/rug 0.25.0

--- .atomist/tests/handlers/command/reaction/ReactSteps.ts
+++ .atomist/tests/handlers/command/reaction/ReactSteps.ts
@@ -1,24 +1,39 @@
-import { Given, When, Then, HandlerScenarioWorld, CommandHandlerScenarioWorld } from "@atomist/rug/test/handler/Core";
-import { HandleResponse, Execute, Respondable, HandleCommand, MappedParameters, Respond, Instruction, Response,
-    HandlerContext, Plan } from "@atomist/rug/operations/Handlers";
+/*
+ * Copyright © 2017 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CommandRespondable, Execute } from "@atomist/rug/operations/Handlers";
+import { CommandHandlerScenarioWorld, Given, Then, When } from "@atomist/rug/test/handler/Core";
+
 import { Http } from "../../../../handlers/HttpApi";
 
 When("([a-zA-Z]+) on ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) is invoked with valid input",
-  (world: HandlerScenarioWorld, handlerName: string, reactableType: string, id: string) => {
-    const parameters = {};
-    parameters[reactableType] = id;
-    invokeReactHandler(world, handlerName, parameters);
-});
+    (world: CommandHandlerScenarioWorld, handlerName: string, reactableType: string, id: string) => {
+        const parameters = {};
+        parameters[reactableType] = id;
+        invokeReactHandler(world, handlerName, parameters);
+    });
 
 When("([a-zA-Z]+) on ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) comment is invoked with valid input",
-  (world: HandlerScenarioWorld, handlerName: string, reactableType: string, id: string) => {
-    const parameters = { comment: "7" };
-    parameters[reactableType] = id;
-    invokeReactHandler(world, handlerName, parameters);
-});
+    (world: CommandHandlerScenarioWorld, handlerName: string, reactableType: string, id: string) => {
+        const parameters = { comment: "7" };
+        parameters[reactableType] = id;
+        invokeReactHandler(world, handlerName, parameters);
+    });
 
-const invokeReactHandler = (world: HandlerScenarioWorld, handlerName: string, specificParameters) => {
-    const w: CommandHandlerScenarioWorld = world as CommandHandlerScenarioWorld;
+const invokeReactHandler = (w: CommandHandlerScenarioWorld, handlerName: string, specificParameters) => {
     const commonParameters = {
         reaction: "+1",
         repo: "testRepo",
@@ -30,31 +45,27 @@ const invokeReactHandler = (world: HandlerScenarioWorld, handlerName: string, sp
     w.invokeHandler(handler, parameters);
 };
 
-Then("respond with a single instruction", (world: HandlerScenarioWorld) => {
-    const w: CommandHandlerScenarioWorld = world as CommandHandlerScenarioWorld;
+Then("respond with a single instruction", (w: CommandHandlerScenarioWorld) => {
     return w.plan().instructions.length === 1;
 });
 
-Then("execute ([a-z\\-]+) instruction", (world: HandlerScenarioWorld, expectedInstructionName: string) => {
-    const w: CommandHandlerScenarioWorld = world as CommandHandlerScenarioWorld;
-    const respondable = w.plan().instructions[0] as Respondable<Execute>;
+Then("execute ([a-z\\-]+) instruction", (w: CommandHandlerScenarioWorld, expectedInstructionName: string) => {
+    const respondable = w.plan().instructions[0] as CommandRespondable<Execute>;
     const instruction = respondable.instruction as Execute;
     const name = instruction.name as string;
     return name === expectedInstructionName;
 });
 
-Then("execute http instruction", (world: HandlerScenarioWorld) => {
-    const w: CommandHandlerScenarioWorld = world as CommandHandlerScenarioWorld;
-    const respondable = w.plan().instructions[0] as Respondable<Execute>;
+Then("execute http instruction", (w: CommandHandlerScenarioWorld) => {
+    const respondable = w.plan().instructions[0] as CommandRespondable<Execute>;
     const instruction = respondable.instruction as Execute;
     const http = instruction.parameters as Http;
     return instruction.name === "http" && http.method === "post" &&
-      http.url === "https://api.github.com/repos/testOwner/testRepo/issues/1/reactions";
+        http.url === "https://api.github.com/repos/testOwner/testRepo/issues/1/reactions";
 });
 
-Then("on success send '(.+)'", (world: HandlerScenarioWorld, expectedSuccessMessage: string) => {
-    const w: CommandHandlerScenarioWorld = world as CommandHandlerScenarioWorld;
-    const respondable = w.plan().instructions[0] as Respondable<Execute>;
+Then("on success send '(.+)'", (w: CommandHandlerScenarioWorld, expectedSuccessMessage: string) => {
+    const respondable = w.plan().instructions[0] as CommandRespondable<Execute>;
     const onSuccessMessage = respondable.onSuccess as any;
     const successMessage = onSuccessMessage.parameters.msg;
     return successMessage === expectedSuccessMessage;
