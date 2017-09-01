6602b90767f2a4532054fd6bfb99006c27cc91cb
Alan Stewart <alankstewart@gmail.com>
2017-09-01 12:59:24 +1000

    Combined add and remove labels #69

--- .atomist/handlers/command/issue/LabelIssue.ts
+++ .atomist/handlers/command/issue/LabelIssue.ts
@@ -20,35 +20,28 @@ import {
     Intent,
     MappedParameter,
     Parameter,
-    ParseJson,
-    ResponseHandler,
     Secrets,
     Tags,
 } from "@atomist/rug/operations/Decorators";
 import {
     CommandPlan,
-    Execute,
     HandleCommand,
     HandlerContext,
-    HandleResponse,
-    Instruction,
     MappedParameters,
-    Respond,
-    Response,
 } from "@atomist/rug/operations/Handlers";
-import { handleErrors, wrap } from "@atomist/rugs/operations/CommonHandlers";
+import { handleErrors } from "@atomist/rugs/operations/CommonHandlers";
 import { execute } from "@atomist/rugs/operations/PlanUtils";
 
-@CommandHandler("AddLabelGitHubIssue", "Add a known label to a GitHub issue")
+@CommandHandler("LabelGitHubIssue", "Add a known label to a GitHub issue")
 @Tags("github", "issues")
 @Secrets("github://user_token?scopes=repo")
-@Intent("add label issue")
-class AddLabelIssueCommand implements HandleCommand {
+@Intent("label issue")
+class LabelIssueCommand implements HandleCommand {
 
     @Parameter({ description: "The issue number", pattern: "^.*$" })
     public issue: number;
 
-    @Parameter({ description: "A label to add to an issue", pattern: "^.*$" })
+    @Parameter({ description: "A label to add to or remove from an issue", pattern: "^.*$" })
     public label: string;
 
     @MappedParameter(MappedParameters.GITHUB_REPOSITORY)
@@ -65,10 +58,10 @@ class AddLabelIssueCommand implements HandleCommand {
 
     public handle(ctx: HandlerContext): CommandPlan {
         const plan = new CommandPlan();
-        const ex = execute("add-label-github-issue", this);
+        const ex = execute("label-github-issue", this);
         plan.add(handleErrors(ex, this));
         return plan;
     }
 }
 
-export let command = new AddLabelIssueCommand();
+export let command = new LabelIssueCommand();
